b1f3db9750d085bdb7c5b23e7a95b07752b80d76
090837 <lindaramadanti11@gmail.com>
2025-06-24 07:22:26 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('HAPPY BIRTHDAY AMIINNNN').split('Live well, I always pray for u here and of course. I always support whatever choice u make, as long as it's good for u. I hope my prayers hug you when my arms can't.LOVE YOU')
     const titleElement = document.getElementById('title');
     let index = 0;
 
